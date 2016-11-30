10956a01ff2e2590ac98db9e6abb396331e09c53
Thom2 <oospamoo@gmx.de>
2016-11-30 21:48:26 +0100

    Removed unnecessary files and code

--- src/tracker_data/Main.java
+++ src/tracker_data/Main.java
@@ -15,9 +15,7 @@ public class Main {
         // this program requires at least two 
         // arguments on the command line 
         if (args.length >= 2) {
-        	if (args[0].compareToIgnoreCase(args[1]) != 0) {
-        		//openFile(args[0], args[1]);
-        		
+        	if (args[0].compareToIgnoreCase(args[1]) != 0) {       		
         		// Parse command line for optional values
         		try {
         			Configuration.instance().initialize(args);
@@ -51,60 +49,4 @@ public class Main {
 			dataFile.write(outFile);
 		}
 	}
-	
-/*
-	private void openFile(String inputFile, String outputFile) {
-		Path dataFilePathIn = Paths.get(System.getProperty("user.dir"), inputFile);
-		Path dataFilePathOut = Paths.get(outputFile);
-
-		Charset charset = Charset.forName("US-ASCII");
-		try (BufferedReader reader = Files.newBufferedReader(dataFilePathIn, charset)) {
-			try (BufferedWriter writer = Files.newBufferedWriter(dataFilePathOut, charset)) {
-				
-			    String line = null;
-			    int lineCounter = 0;
-			    
-			    while ((line = reader.readLine()) != null) {
-			    	System.out.print(line);
-			        
-			        // First two lines are header and column names
-			        if (lineCounter < 2) {
-			        	writer.write(line, 0, line.length());
-			        	writer.newLine();
-			        	
-			        	System.out.println("          =>  " + line);
-			        }
-			        else {
-			        	// Start manipulating data at third line
-			        	String[] values = line.split("\\t");
-			        	
-			        	if (values.length == 3) {
-			        		values[0] = values[0].replace(',', '.');
-			        		values[1] = "0";
-			        		values[2] = values[2].replace(',', '.');
-			        		
-			        		String newLine = String.format("%s\t%s\t%s", values[0], values[1], values[2]);
-			        		
-			        		writer.write(newLine, 0, newLine.length());
-				        	writer.newLine();
-				        	
-				        	System.out.println("      =>  " + newLine);
-			        	}
-			        	else {
-			        		System.out.println("      =>  Error: incorrect count of data fields.");
-			        	}
-			        }
-			        
-			        lineCounter++;
-			        
-			    }
-			} catch (IOException x) {
-			    System.err.format("Output file could not be created. IOException: %s%n", x);
-			}
-		} catch (IOException x) {
-		    System.err.format("Input file not found. IOException: %s%n", x);
-		}
-		
-	}
-*/
 }
